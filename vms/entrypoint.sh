9aa4e0a23cbbf95dcc49fcd3664f2e6c58261d28
marcstu2007 <marcstu2007@gmail.com>
2023-09-25 20:08:04 -0600

    actualización de carpeta

--- vms/entrypoint.sh
+++ vms/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Carga los módulos de kernel
-sudo insmod modulos/cpu_201122934.ko
-sudo insmod modulos/ram_201122934.ko
+sudo insmod modulos/cpu/cpu_201122934.ko
+sudo insmod modulos/ram/ram_201122934.ko
 
 # Ejecuta tu aplicación principal
 # exec "$@"
